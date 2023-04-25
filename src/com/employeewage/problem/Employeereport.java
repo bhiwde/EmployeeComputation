5372a10a8d1f19da75230f956685d91bd4d96d1d
bhiwde <nirmlarameshbhiwde@gmail.com>
2023-04-25 12:50:50 +0530

    [ADDED]:[Nirmla] EmployeeComputation Setup with Welcome Message

---
+++ src/com/employeewage/problem/Employeereport.java
@@ -0,0 +1,12 @@
+package com.employeewage.problem;
+
+/*
+ *  employeereport class handling all operation
+ */
+public class Employeereport {
+
+	public static void main(String[] args) {
+		/*user display message*/
+  System.out.println("Welcome to Employee Wage Computation ");
+	} 
+}

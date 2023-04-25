1285f558702cfa49ee45d81949984f65086e8021
bhiwde <nirmlarameshbhiwde@gmail.com>
2023-04-25 13:40:32 +0530

    [MODIFIED];[Nirmla] Check Employee Attendance

--- src/com/employeewage/problem/Employeereport.java
+++ src/com/employeewage/problem/Employeereport.java
@@ -1,5 +1,7 @@
 package com.employeewage.problem;
 
+import java.util.Random;
+
 /*
  *  employeereport class handling all operation
  */
@@ -8,5 +10,15 @@ public class Employeereport {
 	public static void main(String[] args) {
 		/*user display message*/
   System.out.println("Welcome to Employee Wage Computation ");
+   int attendance = 0;
+   Random random = new Random();
+   attendance = random.nextInt(2);
+   System.out.println("Attendance:"+attendance);
+   if(attendance == 0) {
+	   System.out.println("Employee is Absent");
+   }
+   else {
+	   System.out.println("Employee is Present");
+   }
 	} 
 }

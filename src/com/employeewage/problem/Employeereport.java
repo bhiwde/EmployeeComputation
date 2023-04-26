29e883535d2a141688bf5cd968c3b56ee2263caa
bhiwde <nirmlarameshbhiwde@gmail.com>
2023-04-26 21:43:25 +0530

    [MODIFIED];[Nirmla] Calculating Employee Daily Wage

--- src/com/employeewage/problem/Employeereport.java
+++ src/com/employeewage/problem/Employeereport.java
@@ -10,15 +10,30 @@ public class Employeereport {
 	public static void main(String[] args) {
 		/*user display message*/
   System.out.println("Welcome to Employee Wage Computation ");
-   int attendance = 0;
-   Random random = new Random();
-   attendance = random.nextInt(2);
-   System.out.println("Attendance:"+attendance);
-   if(attendance == 0) {
+   int attendance ;
+  int daily_wage = 0;
+  
+  final int IS_ABSENT = 0;
+  final int WAGE_PER_HR = 20;
+  final int FULL_DAY_HR =8;
+ Random random = new Random();
+  attendance = random.nextInt(2);
+ 
+  System.out.println("Attendance:"+attendance);
+  if(attendance == IS_ABSENT) {
 	   System.out.println("Employee is Absent");
    }
-   else {
-	   System.out.println("Employee is Present");
+   else  {
+		   System.out.println("Employee is Present");
+		   daily_wage   = WAGE_PER_HR * FULL_DAY_HR;
+	  }
+   System.out.println("Employee Daily Wage "+daily_wage);
+   
    }
-	} 
-}
+	}
+
+
+	
+
+
+

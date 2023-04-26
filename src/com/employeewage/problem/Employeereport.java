109ba9bfba9bbed2a49328c4e48a8d75bf11ebbf
bhiwde <nirmlarameshbhiwde@gmail.com>
2023-04-26 22:03:50 +0530

    [MODIFIED]:[Nirmla] Employee Type Checking PartTime and FullTime

--- src/com/employeewage/problem/Employeereport.java
+++ src/com/employeewage/problem/Employeereport.java
@@ -12,26 +12,42 @@ public class Employeereport {
   System.out.println("Welcome to Employee Wage Computation ");
    int attendance ;
   int daily_wage = 0;
+  int emptype ;
   
   final int IS_ABSENT = 0;
   final int WAGE_PER_HR = 20;
   final int FULL_DAY_HR =8;
+  final int FULL_TIME_HR =8;
+  final int PART_TIME_HR = 4;
+  final int IS_PART_TIME = 0;
+  
  Random random = new Random();
   attendance = random.nextInt(2);
- 
+  
   System.out.println("Attendance:"+attendance);
   if(attendance == IS_ABSENT) {
 	   System.out.println("Employee is Absent");
    }
    else  {
 		   System.out.println("Employee is Present");
-		   daily_wage   = WAGE_PER_HR * FULL_DAY_HR;
+	       daily_wage   = WAGE_PER_HR * FULL_DAY_HR;
+	       
+	       emptype = random.nextInt(2);
+            if(emptype == IS_PART_TIME ) {
+			   System.out.println("Employee is Part Time:");
+			   daily_wage = WAGE_PER_HR * PART_TIME_HR;
+		   }
+		   else {
+			   
+			   System.out.println("Employee is Full Time:");
+
+		       daily_wage   = WAGE_PER_HR * FULL_TIME_HR;
 	  }
    System.out.println("Employee Daily Wage "+daily_wage);
    
    }
 	}
-
+}
 
 	
 
